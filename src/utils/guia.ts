34bd0fb7e66328a81848c62907cfe468c13d626f
Diego Araujo <diegolaraujo96@gmail.com>
2023-10-09 19:42:14 -0300

    Added instructions for creating a Business developer app and obtaining app token.

---
+++ src/utils/guia.ts
@@ -0,0 +1,22 @@
+export const guia = `<b>Guía para Crear un Token Permanente para WhatsApp Business</b>
+
+<b>Paso 1: Accedé a la Configuración de Negocio</b>
+1. Iniciá sesión en tu cuenta de [Facebook Business](https://business.facebook.com/).
+2. En el menú principal, seleccioná <b>“Configuración de Negocio”</b>.
+
+<b>Paso 2: Creá un Usuario del Sistema</b>
+1. En la barra lateral izquierda, buscá y seleccioná <b>"Usuarios"</b> y luego <b>“Usuarios del Sistema”</b>.
+2. Hacé clic en <b>“Agregar”</b>.
+3. Seleccioná <b>“Administrador del Sistema”</b> como el rol.
+4. Ingresá el nombre y la dirección de correo electrónico para el nuevo usuario del sistema y confirmá la creación.
+
+<b>Paso 3: Asigná la Aplicación de WhatsApp Business al Usuario del Sistema</b>
+1. Una vez creado el usuario del sistema, hacé clic en su nombre para acceder a su perfil.
+2. Buscá la sección <b>“Aplicaciones</b> y hacé clic en <b>“Asignar Aplicaciones”</b>.
+3. En la lista de aplicaciones disponibles, buscá y seleccioná la aplicación de <b>WhatsApp Business</b>.
+4. Hacé clic en <b>“Guardar”</b>.
+
+<b>Paso 4: Generá el Token Permanente</b>
+1. Aún en el perfil del usuario del sistema, andá a la sección <b>“Tokens de Acceso”</b>.
+2. Hacé clic en <b>“Generar Token”</b>.
+3. Copiá y guardá el token generado en un lugar seguro. Este token será permanente y te permitirá hacer llamadas a la API de WhatsApp Business.`
--- src/utils/handleSendMessage.ts
+++ src/utils/handleSendMessage.ts
@@ -24,6 +24,133 @@ function generateTableHTML(contacts: Contact[]): string {
     return html;
 }
 
+const setupWhatsappVariables =  (contacts: Contact[], message: string): any => Swal.fire({
+    title: 'Ingrese sus datos de Whatsapp Business ',
+    html: `
+<div>
+<div style="display:flex" class="align-center">
+<span>Whatsapp Phone Id</span>
+<button id="infoBtnPhoneId" style="background-color: #007BFF; color: #FFF; border-radius: 50%; padding: 5px 10px; border: none; cursor: pointer; margin-left: 5px;">i</button>
+<input type="text" id="input1" class="swal2-input" placeholder="First Input">
+</div>
+<div style="display:flex" class="align-center">
+<span>Whatsapp Token</span>
+<button id="infoBtnToken" style="background-color: #007BFF; color: #FFF; border-radius: 50%; padding: 5px 10px; border: none; cursor: pointer; margin-left: 5px;">i</button>
+<input type="text" id="input2" class="swal2-input" placeholder="Second Input">
+</div>
+
+
+</div>
+`,
+    focusConfirm: false,
+    didOpen: () => {
+        document.getElementById('infoBtnPhoneId')?.addEventListener('click', () => {
+            Swal.fire({
+                title: 'Creá una Aplicación en Facebook Developers',
+                html: `<ul>
+  <li><strong>Paso 1: Creá una Aplicación en Facebook Developers</strong>
+    <ul>
+      <li class="align-left">Ingresá a <a href="https://developers.facebook.com/apps" target="_blank">developers.facebook.com/apps</a>.</li>
+      <li class="align-left">Hacé clic en el botón “<em>Crear App</em>”.</li>
+      <li class="align-left">Seleccioná “<em>Para Todo en Facebook</em>” y después “<em>Continuar</em>”.</li>
+      <li class="align-left">Completá la información requerida, como el nombre de la app, correo electrónico de contacto y propósito de la app, y luego hacé clic en “<em>Crear ID de App</em>”.</li>
+    </ul>
+  </li>
+    <br/>
+  <li><strong>Paso 2: Configurá WhatsApp en la Aplicación</strong>
+    <ul>
+      <li class="align-left">Una vez creada la app, en el panel lateral izquierdo, buscá y seleccioná “<em>WhatsApp</em>”.</li>
+      <li class="align-left">Seguí las instrucciones para configurar WhatsApp con tu app.</li>
+    </ul>
+  </li>
+    <br/>
+  <li><strong>Paso 3: Obtené el ID del Número de Teléfono</strong>
+    <ul>
+      <li class="align-left">Dentro de la sección de “<em>WhatsApp</em>”, hacé clic en “<em>Configuración de API</em>”.</li>
+      <li class="align-left">En esta sección, podrás ver el ID del número de teléfono. Anotalo o copialo para tus futuras referencias.</li>
+    </ul>
+  </li>
+</ul>`,
+                icon: 'info'
+            })
+            .then(result => {
+                    if (result.isConfirmed) {
+                        return setupWhatsappVariables(contacts, message)
+                    }
+                })
+        })
+        document.getElementById('infoBtnToken')?.addEventListener('click', () => {
+            Swal.fire({
+                title: 'Crear un Token Permanente para WhatsApp Business',
+                html: `<ul>
+<li><b>Para crear un token permanente tuviste que ya haber creado una aplicación en Facebook Developers.</b></li>
+<br/>
+<li>
+<b>Paso 1: Accedé a la Configuración de Negocio</b>
+<ol>
+<li class="align-left">Iniciá sesión en tu cuenta de <a href="https://business.facebook.com/">Facebook Business</a>.</li>
+<li class="align-left">Abrí el menú lateral de usuario de arriba a la izquierda. Tocá la ruedita de configuración de tu cuenta comercial y después andá a <b>“Configuración de Negocio”</b>.</li>
+</ol>
+</li>
+    <br/>
+<li>
+<b>Paso 2: Creá un Usuario del Sistema</b>
+<ol>
+<li class="align-left">En la barra lateral izquierda, buscá y seleccioná <b>"Usuarios"</b> y luego <b>“Usuarios del Sistema”</b>.</li>
+<li class="align-left">Hacé clic en <b>“Agregar”</b>.</li>
+<li class="align-left">Seleccioná <b>“Administrador”</b> como el rol.</li>
+<li class="align-left">Ingresá el nombre para el nuevo usuario del sistema y confirmá la creación.</li>
+</ol>
+</li>
+    <br/>
+<li>
+<b>Paso 3: Asigná la Aplicación de WhatsApp Business al Usuario del Sistema</b>
+<ol>
+<li class="align-left">Una vez creado el usuario del sistema, hacé clic en <b>"Agregar Activos" en el lado derecho</b>.</li>
+<li class="align-left">Buscá la sección <b>“Apps”</b> y buscá y seleccioná la aplicación de <b>WhatsApp Business</b>. En la sección <b>Control Total</b> activá la opción <b>"Administrar app"</b></li>
+<li class="align-left">Hacé clic en <b>“Guardar Cambios”</b>.</li>
+</ol>
+</li>
+    <br/>
+<li>
+<b>Paso 4: Generá el Token Permanente</b>
+<ol>
+<li class="align-left">Aún en la sección Usuarios del sistema, hacé click en <b>"Generar nuevo token"</b>.</li>
+<li class="align-left">Seleccioná tu aplicación de <b>WhatsApp Business</b> y activá la opción "Nunca" para el tiempo de expiración.</li>
+<li class="align-left">Seleccioná los permisos <b>"whatsapp_business_messaging"</b> y <b>"whatsapp_business_management"</b></li>
+<li class="align-left">Guardá y copiá el token generado. Guardá este token en un lugar seguro. Este token va a ser permanente y te va a permitir a vos y a cualquiera que lo tenga enviar mensajes por la API de WhatsApp Business.</li>
+</ol>
+</li>
+</ul>
+`,
+                icon: 'info',
+
+            })
+            .then(result => {
+                    if (result.isConfirmed) {
+                        return setupWhatsappVariables(contacts, message)
+                    }
+                })
+        });
+    },
+    preConfirm: () => {
+        const whatsappPhoneId = (document.getElementById('input1') as HTMLInputElement)?.value;
+        const whatsappToken = (document.getElementById('input2') as HTMLInputElement)?.value;
+        if (!whatsappPhoneId || !whatsappToken) {
+            Swal.showValidationMessage(`Ingrese valores para ambos campos`);
+        }
+        return { whatsappPhoneId, whatsappToken };
+    }
+}).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.setItem('whatsappPhoneId', result.value.whatsappPhoneId)
+            localStorage.setItem('whatsappToken', result.value.whatsappToken)
+            console.log(result.value); // { input1Value: "someValue", input2Value: "someValue" }
+            return handleSendMessage(contacts, message)
+        }
+    });
+
+
 export function handleSendMessage (contacts: Contact[], message: string) {
     return Swal.fire({
         icon: 'warning',
@@ -64,36 +191,7 @@ export function handleSendMessage (contacts: Contact[], message: string) {
     })
         .then(result => {
             if (result.value === "setup") {
-                Swal.fire({
-                    title: 'Ingrese sus datos de Whatsapp Business',
-                    html: `
-                        <div>
-                        <div style="display:flex" class="align-center">
-                        <span>Whatsapp Phone Id</span>
-                        <input type="text" id="input1" class="swal2-input" placeholder="First Input">
-                        </div>
-                        <div style="display:flex" class="align-center">
-                        <span>Whatsapp Token</span>
-                        <input type="text" id="input2" class="swal2-input" placeholder="Second Input">
-                        </div>
-                        </div>
-                    `,
-                    focusConfirm: false,
-                    preConfirm: () => {
-                        const whatsappPhoneId = (document.getElementById('input1') as HTMLInputElement)?.value;
-                        const whatsappToken = (document.getElementById('input2') as HTMLInputElement)?.value;
-                        if (!whatsappPhoneId || !whatsappToken) {
-                            Swal.showValidationMessage(`Ingrese valores para ambos campos`);
-                        }
-                        return { whatsappPhoneId, whatsappToken };
-                    }
-                }).then((result) => {
-                        if (result.isConfirmed) {
-                            localStorage.setItem('whatsappPhoneId', result.value.whatsappPhoneId)
-                            localStorage.setItem('whatsappToken', result.value.whatsappToken)
-                            console.log(result.value); // { input1Value: "someValue", input2Value: "someValue" }
-                        }
-                    });
+                return setupWhatsappVariables(contacts, message)
             }
         })
 }
